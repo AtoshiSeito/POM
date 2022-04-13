7ad977dd2d44c3ebc204359ed5aa15c1e7db0097
AtoshiSeito <ivgor02@gmail.com>
2022-04-13 13:21:23 +0300

    123
    123dfgghh
    
    Co-Authored-By: Yaroslav <yaryarikyar@ya.ru>

---
+++ app/src/main/java/com/example/kotlinsprogramm/ui/Fragments/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinsprogramm.ui.Fragments
+
+import com.example.kotlinsprogramm.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
